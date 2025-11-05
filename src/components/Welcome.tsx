6954bdac991499077a930f14a49b268434321997
Muhammad Lihaz Ali <lehazali123@gmail.com>
2025-11-05 07:26:34 -0800

    reduce margin

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,8 +1,8 @@
 const Welcome = () => {
   return (
-    <section className="bg-primary py-16 md:py-20">
+    <section className="bg-primary py-5 md:py-10">
       <div className="container mx-auto px-6">
-        <p className="text-white text-base md:text-lg leading-relaxed max-w-5xl">
+        <p className="text-white  text-start text-base md:text-lg leading-relaxed max-w-5xl">
           Welcome to CandySpace, the premier platform where accomplished gentlemen connect with driven young Filipina women pursuing their dreams. Our community is built on mutual respect, support, and shared aspirations. Whether you're here to offer mentorship, guidance and financial support, or to find meaningful connections that inspire personal growth, you've come to the right place. Together, we foster opportunities that empower women and their ambitions, and celebrate wisdom and generosity—creating relationships that are enriching, genuine, and impactful.
         </p>
       </div>
